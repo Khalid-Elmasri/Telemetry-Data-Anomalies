f02e06a8a9bcecd0d7a39b668a456f143b269dfa
Khalid Elmasri <u04ke24@abdn.ac.uk>
2026-09-07 15:13:10 +0100

    Add files via upload

---
+++ Bright.py
@@ -0,0 +1,15 @@
+#
+import numpy as np
+import pandas as pd
+
+#read data
+parse=pd.read_csv("C:/Users/PC/Downloads/telemetry_data(in).csv")
+
+#Find anomoly
+anomaly=parse[
+    (parse["temperature_c"]>85) |
+    (parse["vibration_mm_s"]>15)]
+
+print("Turbines with anomalies:")
+print(anomaly["turbine_id"].unique())
+
